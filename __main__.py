8118ab9e6b0f557d0eb098286f0fbee3e9719272
RIscRIpt <richard@riscript.com>
2019-03-21 23:51:08 +0100

    simplify search

--- __main__.py
+++ __main__.py
@@ -3,13 +3,13 @@ import itertools
 import more_itertools
 
 def factorize_into_digits(n):
-    primes = [2, 3, 5, 7]
+    digits = list(range(9, 1, -1))
     factors = []
     target = n
-    for prime in primes:
-        while n % prime == 0:
-            n //= prime
-            factors.append(prime)
+    for digit in digits:
+        while n % digit == 0:
+            n //= digit
+            factors.append(digit)
         if n == 1:
             break
     result = 1
@@ -19,15 +19,23 @@ def factorize_into_digits(n):
         return []
     return factors
 
+def factors_of_factors(factors):
+    primes = [2, 3]
+    yield tuple(factors)
+    for i in range(len(factors)):
+        for prime in primes:
+            if factors[i] > 3 and factors[i] % prime == 0:
+                new_factors = factors.copy()
+                factor = factors[i]
+                new_factors.remove(factor)
+                new_factors.append(factor // prime)
+                new_factors.append(prime)
+                new_factors.sort()
+                yield from factors_of_factors(new_factors)
+
 def yield_numbers_from(factors):
     for fl in more_itertools.distinct_permutations(factors):
         yield int("".join(map(str, fl)))
-
-def multiply_lists(to_multiply, factors, mul_list):
-    result = to_multiply.copy()
-    for i in range(len(factors)):
-        result[mul_list[i]] *= factors[i]
-    return result
 
 def mul_result(n):
     steps = [n]
@@ -51,23 +59,8 @@ def find_mul_with_result(n):
         factors = factorize_into_digits(n)
         if len(factors) == 0:
             return
-        for r in range(math.ceil(n ** (1/7)), len(factors) + 1):
-            if r == len(factors):
-                yield from yield_numbers_from(factors)
-            else:
-                mul_comb_len = len(factors) - r
-                yielded_factors = set()
-                for mul_comb in set(itertools.combinations(factors, mul_comb_len)):
-                    new_factors = factors.copy()
-                    for value in mul_comb:
-                        new_factors.remove(value)
-                    for mul_list in itertools.product(range(len(new_factors)), repeat=len(mul_comb)):
-                        multiplied_factors = multiply_lists(new_factors, mul_comb, mul_list)
-                        fz_multiplied_factors = frozenset(multiplied_factors)
-                        if fz_multiplied_factors not in yielded_factors:
-                            yielded_factors.add(fz_multiplied_factors)
-                            if max(multiplied_factors) < 10:
-                                yield from yield_numbers_from(multiplied_factors)
+        for fs in set(factors_of_factors(factors)):
+            yield from yield_numbers_from(fs)
 
 checked = set()
 
